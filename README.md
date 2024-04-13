a40240f0d5523ee328c1962543d334f3d00c87f1
Olisa Nweze <nwezeolisa@gmail.com>
2024-04-12 20:30:48 -0500

    random users generated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## JobsPool
+
+This is a sample website that uses the randomuser API to generate users.
+More functioniality coming soon!
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,34 @@
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*=======================================================*/
 /*                                                       */
-/*  Olisa Nweze (2024)                                   */
+/*  olisa nweze (2024)                                   */
 /*  github.com/olisanweze                                */
 /*                                                       */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*=======================================================*/
 
 'use strict';
 
-import {  select, listen  } from './utils.js';
+import {  select, listen, create  } from './utils.js';
+
+/*=======================================================*/
+/*  global variables                                     */
+/*=======================================================*/
 
 const login = select('.login-button');
 const userEmail = select('.user-email');
 const userPassword = select('.user-password');
 const incorrectAlert = select('.incorrect-details');
+const peopleSuggestions = select('.people-section');
+const URL = 'https://randomuser.me/api/?nat=CA&results=10&seed=same';
+
+const options = {
+  method: 'GET',
+  headers: {  'Content-Type': 'application/JSON; charset=UTF-8'  },
+  mode: 'cors'
+}
+
+/*=======================================================*/
+/*  functions                                            */
+/*=======================================================*/
 
 function setLoginCredentials() {
   localStorage.setItem('username', 'andrespecht@email.com');
@@ -26,7 +42,7 @@ function checkLoginCredentials(username, password) {
   if (username === storedUserName && password === storedPassword) {
     window.location.href = './home.html';
   } else {
-    incorrectAlert.classList.remove('incorrect-none');
+    incorrectAlert.style.visibility = 'visible';
   }
 }
 
@@ -37,12 +53,61 @@ function allowUserAccess() {
   checkLoginCredentials(username, password);
 }
 
-function hideAlert() {
-  incorrectAlert.classList.add('incorrect-none');
+async function getUsers(endpoint) {
+  try {
+    const result = await fetch(endpoint, options);
+
+    if (!result.ok) {
+      throw new Error(`${result.statusText} (${result.status})`);
+    }
+
+    const data = await result.json();
+    genUsersInformation(data);
+  } catch(error) {
+  }
 }
 
+getUsers(URL);
 
+async function genUsersInformation(data) {
+  const users = data.results.map(user => ({
+    profilePicture: user.picture.large,
+    name: `${user.name.first} ${user.name.last}`,
+    city: user.location.city
+  }));
+
+  buildOtherUsers(users);
+}
+
+function buildOtherUsers(users) {
+  users.forEach(user => {
+    const userDiv = create('div');
+    userDiv.classList.add('flex');
+
+    const img = create ('div');
+    img.classList.add('profile-img-people');
+    img.style.background = `#fff url(${user.profilePicture}) center / cover no-repeat`
+    userDiv.appendChild(img);
+
+    const userDetails = create('div');
+    const name = create('h5');
+    name.textContent = user.name;
+    userDetails.appendChild(name);
+
+    const city = create('p');
+    city.classList.add('people-suggestions-city');
+    city.classList.add('grey');
+    city.textContent = user.city;
+    userDetails.appendChild(city);
+    userDiv.appendChild(userDetails);
+    peopleSuggestions.appendChild(userDiv);
+  })
+}
+
+/*=======================================================*/
+/*  event listeners                                      */
+/*=======================================================*/
 
 listen('load', window, setLoginCredentials);
-listen('load', window, hideAlert);
 listen('click', login, allowUserAccess);
+listen('load', window, getUsers);
